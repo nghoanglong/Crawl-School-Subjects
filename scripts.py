7aa7b664ad75d3204ebbb4a557a3699188c4b197
= <=>
2020-03-17 19:30:33 +0700

    Create scraping data

---
+++ scripts.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+def collect_data(numbers, subjects, summaries):
+    data = []
+
+    for idx, item in enumerate(numbers):
+        if idx > 0:
+            number = int(numbers[idx].getText())
+            subject = subjects[idx].getText()
+            summary = summaries[idx].getText()
+            data.append(
+                {'number': number, 'subject': subject, 'summary': summary})
+    print('Done crawl')
+
+    try:
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False)
+    except IOError as err:
+        print(err)
+
+
+def crawl_data():
+    res = requests.get('https://daa.uit.edu.vn/content/bang-tom-tat-mon-hoc')
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    numbers = soup.select("td[width='43'] > p")
+    subjects = soup.select("td[width='157'] > p")
+    summaries = soup.select("td[width='643'] > p")
+
+    collect_data(numbers, subjects, summaries)
+
+crawl_data()
